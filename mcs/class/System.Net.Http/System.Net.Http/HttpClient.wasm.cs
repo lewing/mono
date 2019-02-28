17bd58544a8db157a70bb405c995c940bf0a9c8b
Larry Ewing <lewing@microsoft.com>
2019-02-28 16:44:38 -0600

    Remove static field in HttpClient

--- mcs/class/System.Net.Http/System.Net.Http/HttpClient.wasm.cs
+++ mcs/class/System.Net.Http/System.Net.Http/HttpClient.wasm.cs
@@ -6,9 +6,6 @@ namespace System.Net.Http
 {
     public partial class HttpClient
     {
-
-        private static Func<HttpMessageHandler> GetHttpMessageHandler;
-
         public HttpClient()
             : this(GetDefaultHandler(), true)
         {
@@ -16,36 +13,23 @@ namespace System.Net.Http
 
         static HttpMessageHandler GetDefaultHandler()
         {
+            Type type = Type.GetType("WebAssembly.Net.Http.HttpClient.WasmHttpMessageHandler, WebAssembly.Net.Http");
+            if (type == null)
+                return GetFallback ("Invalid WebAssembly Module? Cannot find WebAssembly.Net.Http.HttpClient.WasmHttpMessageHandler");
 
-            if (GetHttpMessageHandler == null)
-            {
-                Type type = Type.GetType("WebAssembly.Net.Http.HttpClient.WasmHttpMessageHandler, WebAssembly.Net.Http");
-                if (type == null)
-                    return GetFallback ("Invalid WebAssembly Module? Cannot find WebAssembly.Net.Http.HttpClient.WasmHttpMessageHandler");
+            MethodInfo method = type.GetMethod("GetHttpMessageHandler", BindingFlags.Static | BindingFlags.NonPublic);
+            if (method == null)
+                return GetFallback ("Your WebAssembly version does not support obtaining of the custom HttpClientHandler");
 
-                MethodInfo method = type.GetMethod("GetHttpMessageHandler", BindingFlags.Static | BindingFlags.NonPublic);
-                if (method == null)
-                    return GetFallback ("Your WebAssembly version does not support obtaining of the custom HttpClientHandler");
+            object ret = method.Invoke(null, null);
+            if (ret == null)
+                return GetFallback ("WebAssembly returned no custom HttpClientHandler");
 
-                object ret = method.Invoke(null, null);
-                if (ret == null)
-                    return GetFallback ("WebAssembly returned no custom HttpClientHandler");
-
-                var handler = ret as HttpMessageHandler;
-                if (handler == null)
-                    return GetFallback ($"{ret?.GetType()} is not a valid HttpMessageHandler");
-                    
-                return handler;
-
-            }
-            else
-            {
-                var handler = GetHttpMessageHandler();
-                if (handler == null)
-                    return GetFallback($"Custom HttpMessageHandler is not valid");
-
-                return handler;
-            }                
+            var handler = ret as HttpMessageHandler;
+            if (handler == null)
+                return GetFallback ($"{ret?.GetType()} is not a valid HttpMessageHandler");
+                
+            return handler;             
         }
 
         static HttpMessageHandler GetFallback(string message)
